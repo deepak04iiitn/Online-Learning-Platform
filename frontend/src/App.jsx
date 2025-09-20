66a8897f2ed8607be32a9528b2690693ae56a476
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-09-20 16:18:40 +0530

    feat: setup the react router dom in the frontend with basic skeleton of the same pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 export default function App() {
   return (
-    <div>
-      Welcome to world
-    </div>
-  )
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex-grow">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
